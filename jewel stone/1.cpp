099f21ebb315020a4234399f753ca194f5893582
ABHINAV-JHA-27 <75615092+ABHINAV-JHA-27@users.noreply.github.com>
2021-10-03 01:58:48 +0530

    commit 1

---
+++ jewel stone/1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int sum=0;
+        for(int i=0;i<jewels.length();i++){
+            sum += count(stones.begin(),stones.end(),jewels[i]);
+        }
+        return sum;
+    }
+};
